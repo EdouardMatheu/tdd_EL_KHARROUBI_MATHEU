3fa92b53b23feb24898ba36c099b91f21fd8bfd7
EdouardMatheu <edouardmatheu@gmail.com>
2022-01-04 11:37:09 +0100

    add is_geo_progression function

--- funcs.py
+++ funcs.py
@@ -35,3 +35,9 @@ def std_dev(l):
 		s += (e - mu)*(e - mu)
 	s /= len(l)
 	return math.sqrt(s)
+
+def is_geo_progression(l):
+	for i in range(1,len(l)):
+		if (l[i]%l[i-1] != 0):
+			return False 
+	return True
